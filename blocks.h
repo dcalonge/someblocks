3ebcbe3c6a6300980c1407bb106740369f7374fa
dcalonge <dcalonge01@gmail.com>
2024-09-01 00:33:02 +0200

    update

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 static const Block blocks[] = {
 //   Icon    Command                          Update Interval     Update Signal
     { "", "/home/daniel/scripts/checkup.sh",                 3660,               0 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/volume",                               0,                10 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/brightness",                           0,                20 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/wifi",                              5,               0 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/battery",                              10,               0 },
-    { "| ",    "date '+%d/%m %H:%M' ",        30,                0 },
+    { "",    "/home/daniel/mis_repos/someblocks/blocks/volume",                               0,                10 },
+    { "",    "/home/daniel/mis_repos/someblocks/blocks/brightness",                           0,                20 },
+    { "",    "/home/daniel/mis_repos/someblocks/blocks/wifi",                              5,               0 },
+    { "",    "/home/daniel/mis_repos/someblocks/blocks/battery",                              10,               0 },
+    { "",    "date '+%d/%m %H:%M' ",        30,                0 },
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
-static unsigned int delimLen = 3;
+static char delim[] = "   ";
+static unsigned int delimLen = 5;
