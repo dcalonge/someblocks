e4e0de541447859489ecfd3a309dfb1606334cd1
dcalonge <dcalonge01@gmail.com>
2024-08-31 19:44:48 +0200

    update

--- blocks.h
+++ blocks.h
@@ -1,8 +1,8 @@
 static const Block blocks[] = {
 //   Icon    Command                          Update Interval     Update Signal
     { "", "/home/daniel/scripts/checkup.sh",                 3660,               0 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/volume",                               2,                55 },
-    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/brightness",                           2,                44 },
+    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/volume",                               0,                10 },
+    { "| ",    "/home/daniel/mis_repos/someblocks/blocks/brightness",                           0,                20 },
     { "| ",    "/home/daniel/mis_repos/someblocks/blocks/wifi",                              5,               0 },
     { "| ",    "/home/daniel/mis_repos/someblocks/blocks/battery",                              60,               0 },
     { "| ",    "date '+%d/%m  %H:%M'",        30,                0 },
