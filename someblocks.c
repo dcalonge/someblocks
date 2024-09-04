c01613950d614495d65426ee12132d04f260c3fe
dcalonge <dcalonge01@gmail.com>
2024-09-04 15:58:04 +0200

    update

--- someblocks.c
+++ someblocks.c
@@ -8,7 +8,6 @@
 #include<signal.h>
 #define SIGPLUS			SIGRTMIN
 #define SIGMINUS		SIGRTMIN
-#endif
 #define LENGTH(X)               (sizeof(X) / sizeof (X[0]))
 #define CMDLENGTH		50
 #define MIN( a, b ) ( ( a < b) ? a : b )
